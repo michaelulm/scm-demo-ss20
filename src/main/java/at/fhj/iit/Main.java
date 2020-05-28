e3a8850c95df15fd3ecd955295d4f2f14c2eec88
Michael Ulm <michael.ulm@mea-it.services>
2020-05-28 10:16:57 +0200

    commented unused tests in Main Method

--- src/main/java/at/fhj/iit/Main.java
+++ src/main/java/at/fhj/iit/Main.java
@@ -1,11 +1,28 @@
 package at.fhj.iit;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args){
 
-        Liquid l = new Liquid("Wein", 0.125, 13);
-        System.out.println(l.getName());
-        System.out.println(l.getVolume());
+        // = weiterhin einstiegspunkt in Ihre Applikation
+
+
+        // "old" test cases are deprecated -> JUnit Test -> Best Practice
+//        Liquid l = new Liquid("Wein", 0.125, 13);
+//        Liquid w = new Liquid("Wasser", 0.3, 0);
+//
+//        List<Liquid> i = new ArrayList<>();
+//        i.add(l);
+//        i.add(w);
+//
+//        System.out.println(l.getName());
+//        System.out.println(l.getVolume());
+//
+//        System.out.println(w.getName());
+//        System.out.println(w.getVolume());
+
     }
 }
